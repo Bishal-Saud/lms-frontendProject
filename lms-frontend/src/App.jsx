6c3ea5fbe4f6e1525a3146a3cb2f85322e06b5e0
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2023-10-24 11:30:57 +0545

    added 404 not found page

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -3,12 +3,14 @@ import "./app.css";
 
 import HomePage from "./Pages/Homepage";
 import AboutUs from "./Pages/AboutUs";
+import NotFound from "./Pages/NotFound";
 function App() {
   return (
     <>
      <Routes>
       <Route path="/" element={<HomePage/>}></Route>
       <Route path="/about" element={<AboutUs/>}></Route>
+      <Route path="*" element={<NotFound/>}></Route>
      </Routes>
     </>
   );
