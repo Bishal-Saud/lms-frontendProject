8dc2b657d2d792fb443841212006ead36e5b4dbf
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2023-11-24 13:16:16 +0545

    lecturesSlice added

--- lms-frontend/src/Redux/store.js
+++ lms-frontend/src/Redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducers from "./slices/authSlice";
 import courseSliceReducers from "./slices/CourseSlice.js";
 import razorpaySliceReducer from "./slices/RazorPaySlice.js";
+import lectureSlice from "./slices/LectureSlice.js";
 const store = configureStore({
   reducer: {
     auth: authSliceReducers,
     course: courseSliceReducers,
     razorpay: razorpaySliceReducer,
+    lecture: lectureSlice,
   },
   devTools: true,
 });
