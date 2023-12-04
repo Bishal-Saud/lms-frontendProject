5864e4602d9fb5a82ddcae35c9af5e32c7477d84
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2023-12-04 15:54:36 +0545

    All errors solved and created contact page

--- lms-frontend/src/Pages/ContactUs.jsx
+++ lms-frontend/src/Pages/ContactUs.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
-import HomeLayout from "../Layouts/HomeLayout";
-import toast from "react-hot-toast";
-import { isEmail } from "../Helpers/RegexMatcher";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
 import axiosInstance from "../Helpers/axiosInstance";
+import Layout from "../Layouts/HomeLayout";
 
-function ContactUs() {
+const Contact = () => {
   const [userInput, setUserInput] = useState({
     name: "",
     email: "",
@@ -43,7 +42,7 @@ function ContactUs() {
       });
       const response = await res;
 
-      // clearing the input fields after successful submission of form
+      // clearing the input fields after successfull submission of form
       if (response?.data?.success) {
         setUserInput({
           name: "",
@@ -57,66 +56,67 @@ function ContactUs() {
   };
 
   return (
-    <HomeLayout>
-      <div className="flex items-center justify-center h-screen">
+    <Layout>
+      <div className="flex items-center justify-center h-[100vh]">
         <form
-          noValidate
           onSubmit={handleFormSubmit}
-          className="flex flex-col items-center justify-center gap-5 p-5 rounded-md text-white shadow-[0_0_10px_black] w-[22rem]"
+          className="flex flex-col items-center justify-center gap-2 p-5 rounded-md text-white shadow-[0_0_10px_black] w-[22rem]"
         >
           <h1 className="text-3xl font-semibold">Contact Form</h1>
           <div className="flex flex-col w-full gap-1">
-            <label htmlFor="name" className="text-xl font-semibold">
+            <label className="text-xl font-semibold" htmlFor="name">
               Name
             </label>
             <input
               className="bg-transparent border px-2 py-1 rounded-sm"
-              type="text"
               id="name"
+              type="text"
               name="name"
               placeholder="Enter your name"
-              onChange={handleInputChange}
               value={userInput.name}
+              onChange={handleInputChange}
             />
           </div>
+
           <div className="flex flex-col w-full gap-1">
-            <label htmlFor="email" className="text-xl font-semibold">
+            <label className="text-xl font-semibold" htmlFor="email">
               Email
             </label>
             <input
               className="bg-transparent border px-2 py-1 rounded-sm"
-              type="text"
               id="email"
+              type="email"
               name="email"
-              placeholder="Enter your email"
-              onChange={handleInputChange}
+              placeholder="Enter the email"
               value={userInput.email}
+              onChange={handleInputChange}
             />
           </div>
+
           <div className="flex flex-col w-full gap-1">
-            <label htmlFor="message" className="text-xl font-semibold">
+            <label className="text-xl font-semibold" htmlFor="message">
               Message
             </label>
             <textarea
-              className="bg-transparent border px-2 py-1 rounded-sm h-40 resize-none"
-              type="text"
-              id="message"
+              className="bg-transparent border px-2 py-1 rounded-sm resize-none h-40"
               name="message"
+              id="message"
               placeholder="Enter your message"
-              onChange={handleInputChange}
               value={userInput.message}
-            />
+              onChange={handleInputChange}
+            ></textarea>
           </div>
+
           <button
+            className="w-full bg-yellow-600 hover:bg-yellow-500 transition-all ease-in-out duration-300 rounded-sm py-2 font-semibold text-lg cursor-pointer"
             type="submit"
-            className="w-full bg-yellow-600 hover:bg-yellow-500 transition-all ease-in-out duration-300 rounded-sm p-2 font-semibold text-xl cursor-pointer"
           >
             Submit
           </button>
         </form>
       </div>
-    </HomeLayout>
+    </Layout>
   );
-}
+};
 
-export default ContactUs;
+export default Contact;
